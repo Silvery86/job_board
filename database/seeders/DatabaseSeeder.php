8ea30824cb815c1a592ff5e3353b683a2bba40aa
giang <giang8692@gmail.com>
2025-03-09 13:01:15 +0700

    Update: Seed user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            "name"=> "Admin",
+            "email"=> "admin@gmail.com",
+        ]);
         User::factory(300)->create();
         $users = User::all()->shuffle();
 
