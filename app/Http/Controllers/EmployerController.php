c48a0e850aac8f4d7164b939966a57d59c50efe2
giang <giang8692@gmail.com>
2025-03-13 23:35:18 +0700

    Add: Create employer model, controller, route

---
+++ app/Http/Controllers/EmployerController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employer;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+
+class EmployerController extends Controller
+{
+    use Authorizable;
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('employer.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Auth::user()->employer()->create(
+            $request->validate([
+                'company_name' => 'required|min:3|unique:employers,company_name'
+            ])
+        );
+
+        return redirect()->route('jobs.index')->with("success" , "Your employer account have created");
+    }
+
+
+}
--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -11,6 +11,9 @@ class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+    protected $fillable = [
+        'company_name'
+    ];
 
     public function jobs(): HasMany
     {
